99ff471063cf63d1e72613985f992691b28a9652
SardineFish <Sardine@live.cn>
2017-12-05 23:49:53 +0800

    Add scripts

--- UAV Sim/Assets/UAV/UAV.cs
+++ UAV Sim/Assets/UAV/UAV.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class UAV : MonoBehaviour {
-    public List<GameObject> Pivots = new List<GameObject>();
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public List<GameObject> PivotsObject = new List<GameObject>();
+
+    public List<Pivot> Pivots = new List<Pivot>();
+
+    new Rigidbody rigidbody;
+    // Use this for initialization
+	[ExecuteInEditMode]
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        Pivots.Clear();
+		foreach (var obj in PivotsObject)
+		{
+            var pivot = obj.GetComponent<Pivot>();
+			if(!pivot)
+			{
+                pivot = obj.AddComponent<Pivot>();
+            }
+            Pivots.Add(pivot);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
